e741d56fd5aef042a208e97e5725dbaed2ffcc77
BT-dherreros <daniel.herreros@braintec-group.com>
2018-03-16 12:08:23 +0100

    Second update

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -7,14 +7,14 @@
     'description' : """
                         Open Academy module for practical training exercises
                     """,
-    #data files to be loaded ALWAYS
-    'data' : [
-        'views/mydata.xml',
-    ],
-    #data files to be loaded ONLY while in demo
-    'demo' : [
-        'views/mydemodata.xml'
-    ],
+    # #data files to be loaded ALWAYS
+    # 'data' : [
+    #     'views/mydata.xml',
+    # ],
+    # #data files to be loaded ONLY while in demo
+    # 'demo' : [
+    #     'views/mydemodata.xml'
+    # ],
     #should we autoinstall the module
     'autoinstall' : False,
 
