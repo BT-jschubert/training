a67cc715867dd6341171e8ccfd4b5382566c8f98
BT-dherreros <daniel.herreros@braintec-group.com>
2018-03-16 12:02:53 +0100

    First update

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name' : 'OpenAcademy',
+    'version'  : '0.01',
+    'depends' : ['base'],
+    'author' :  'Daniel Herreros',
+    'category' : 'Category',
+    'description' : """
+                        Open Academy module for practical training exercises
+                    """,
+    #data files to be loaded ALWAYS
+    'data' : [
+        'views/mydata.xml',
+    ],
+    #data files to be loaded ONLY while in demo
+    'demo' : [
+        'views/mydemodata.xml'
+    ],
+    #should we autoinstall the module
+    'autoinstall' : False,
+
+}
