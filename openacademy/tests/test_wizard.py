2bed554d2cffe83cb4a030d32c6d2b2358681f7f
BT-dherreros <daniel.herreros@braintec-group.com>
2018-03-23 17:09:35 +0100

    [btxxxx][FIX] Changed some code to work with xml_ids instead of doing searches in the env

--- openacademy/tests/test_wizard.py
+++ openacademy/tests/test_wizard.py
@@ -9,10 +9,12 @@ class TestWizard(common.TransactionCase):
         self.initial_session_1 = self.env['openacademy.session'].search([('name', '=', 'Session_test1')])
         self.initial_session_2 = self.env['openacademy.session'].search([('name', '=', 'Session_test2')])
         # add attendess through wizard
-        self.agrolait_id = self.env['res.partner'].search([('name', '=', 'Agrolait')])['id']
+        self.partner_1 = self.env.ref('base.res_partner_1')
+        self.partner_2 = self.env.ref('base.res_partner_2')
         self.wizard_obj = self.env['openacademy.wizard']
 
-        self.wizard = self.wizard_obj.create({'partner_relation': [(4, self.agrolait_id)],
+        self.wizard = self.wizard_obj.create({'partner_relation': [(4, self.partner_1.id),
+                                                                   (4, self.partner_2.id)],
                             'session_relations': [(4, self.initial_session_1.id),
                                                   (4, self.initial_session_2.id)]})
         pass
@@ -25,6 +27,8 @@ class TestWizard(common.TransactionCase):
         #check sessions final attendess
         attendee_list_ses1 = self.initial_session_1.attendees_ids
         attendee_list_ses2 = self.initial_session_2.attendees_ids
-        self.assertEqual(attendee_list_ses1.id, self.agrolait_id, 'Not in session 1')
-        self.assertEqual(self.agrolait_id, attendee_list_ses1.id, 'Not in session 2')
+        self.assertIn(self.partner_1, attendee_list_ses1, 'Not p1 in session 1')
+        self.assertIn(self.partner_2, attendee_list_ses1, 'Not p2 in session 1')
+        self.assertIn(self.partner_1, attendee_list_ses2, 'Not p1 in session 2')
+        self.assertIn(self.partner_2, attendee_list_ses2, 'Not p2 in session 2')
 
