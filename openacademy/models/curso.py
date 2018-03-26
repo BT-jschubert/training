d7c1fb2df0e7eaf5d1372f421c0664e80d81ce93
BT-dherreros <daniel.herreros@braintec-group.com>
2018-03-26 17:15:56 +0200

    [btxxxx][XXX] Failed to adapt to the new API

--- openacademy/models/curso.py
+++ openacademy/models/curso.py
@@ -5,6 +5,7 @@ class Curso(models.Model):
     _rec_name = "nombre"
     _sql_constraints = [('check_different_title_description', 'CHECK(nombre != descripcion)', 'Name and description cant be the same!'),
                         ('check_unique_title', 'UNIQUE(nombre)','Name should be unique')]
+
     nombre = fields.Text(required=True,translate=True)
     descripcion = fields.Text(default='Descripcion generica')
     responsible_id=fields.Many2one('res.users')
@@ -20,12 +21,19 @@ class Curso(models.Model):
             for session in course.session_ids:
                 full_sessions = session.search([('related_Course_id','=',course.id),
                                 ('percentage_of_seats_taken','=',100)])
-                # result = full_sessions == session
-                # course.has_full_sessions = result
+                result = full_sessions == session
+                course.has_full_sessions = result
                 full_session_ids = [x.id for x in full_sessions]
                 session_ids = [x.id for x in course.session_ids]
                 result = list(set(full_session_ids).intersection(set(session_ids)))
-                course.has_full_sessions = result == session_ids
+
+                # course.has_full_sessions = result == session_ids
+                ## New API implementation not working properlly
+                # full_sessions &= course.session_ids
+                # if full_sessions &= course.session_ids is None:
+                #     course.has_full_sessions = False
+                # else:
+                #     course.has_full_sessions = True
 
     def _full_session_search(self, operator, value):
         course_ids = []
@@ -38,7 +46,10 @@ class Curso(models.Model):
                 result = list(set(full_session_ids).intersection(set(session_ids)))
                 if result == session_ids:
                     course_ids.append(course.id)
+                    ## New API implementation not working properlly
+                # full_sessions |= course.session_ids
         return [('id', 'in', list(set(course_ids)))]
+        # return [('id', 'in', full_sessions )]
 
     @api.multi
     def copy(self, default=None):
