64b3f631ae95ed63974d7f8ed1c5903b748714bf
Alessandro Mascherpa <alessandro.mascherpa@braintec-group.com>
2019-06-05 19:24:24 +0200

    added python and sql constraints

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -3,6 +3,11 @@ from odoo import models, fields
 class Course(models.Model):
     _name = 'openacademy.course'
     _rec_name = 'title'
+    _sql_constraints = [
+        ('title_description_check', 'CHECK(title != description)', "Title and description can not be equal"),
+        ('title_unique_check', 'UNIQUE(title)', "Title must be unique"),
+    ]
+
     title = fields.Char()
     description = fields.Char()
     responsible = fields.Many2one("res.users")
