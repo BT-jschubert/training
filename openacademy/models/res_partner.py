9fa9d3fb440be49c11fb968cd8ddf21ead0f5d91
Alessandro Mascherpa <alessandro.mascherpa@braintec-group.com>
2019-06-10 12:30:48 +0200

    Fix typo in the table for the relationship session_attendKee_rel

--- openacademy/models/res_partner.py
+++ openacademy/models/res_partner.py
@@ -5,4 +5,4 @@ class resPartner(models.Model):
     _inherit = 'res.partner'
 
     is_instructor = fields.Boolean(string="Instructor")
-    sessions = fields.Many2many("openacademy.session", relation="session_attendKee_rel", column1="partner", column2="session", string="Sessions")
+    sessions = fields.Many2many("openacademy.session", relation="session_attendee_rel", column1="partner", column2="session", string="Sessions")
