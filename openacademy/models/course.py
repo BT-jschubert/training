4807ceace5b6e8bbda3f62d0b24da8ca179844cf
Alessandro Mascherpa <alessandro.mascherpa@braintec-group.com>
2019-06-10 15:40:04 +0200

    Training exercise: Search field to get courses with all full sessions

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -12,3 +12,19 @@ class Course(models.Model):
     description = fields.Char(string="Description")
     responsible = fields.Many2one("res.users", string="Responsible")
     sessions = fields.One2many("openacademy.session", "course", string="Sessions")
+    has_all_sessions_full = fields.Boolean(string="Has all sessions full", compute=lambda self: None, search='_search_has_all_sessions_full')
+
+
+    def _search_has_all_sessions_full(self, operator, value):
+        records = self.env['openacademy.course'].search([('sessions', '!=', False)])
+        ids = []
+        for r in records:
+            for s in r.sessions:
+                sessions_full = True
+                if s.seats > len(s.attendees):
+                    sessions_full = False
+                    break
+            if sessions_full:
+                ids.append(r.id)
+
+        return[('id', 'in', ids)]
