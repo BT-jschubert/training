189a166d1e7f22b762d6401096cca3e21ab36938
BT-jschubert <s:QI8CVH>
2020-01-15 11:18:00 +0100

    WIP openacademy Module

--- addons/openacademy/models/course.py
+++ addons/openacademy/models/course.py
@@ -1,10 +1,24 @@
+from odoo import api, fields, models
 
-from odoo import api,fields,models
 
 class Course(models.Model):
-    _name='course'
+    _name = 'course'
     _description = 'Courses'
     name = fields.Text(string='Name')
     description = fields.Html(string='Description')
-    responsible_id = fields.Many2one(string='Responsible',comodel_name='res.users')
-    sessions = fields.One2many(comodel_name='session',inverse_name='course_id')
+    responsible_id = fields.Many2one(string='Responsible', comodel_name='res.users')
+    sessions = fields.One2many(comodel_name='session', inverse_name='course_id')
+    full_course = fields.Boolean(string='Course is Full', compute='_compute_full_course', store=True)
+
+    #
+    @api.depends('sessions', 'sessions.seats_taken')
+    def _compute_full_course(self):
+        for record in self:
+            if record.sessions:
+                is_full = True
+                for session in record.sessions:
+                    if session.seats_taken < 100:
+                        is_full &= False
+                        if not is_full:
+                            break
+                record.full_course = is_full
--- addons/openacademy/models/session.py
+++ addons/openacademy/models/session.py
@@ -1,14 +1,40 @@
+from odoo import api, fields, models
+from datetime import timedelta
 
-from odoo import api,fields,models
 
 class Session(models.Model):
-    _name='session'
+    _name = 'session'
     _description = 'Sessions'
 
     name = fields.Text(string='Name')
-    start_date = fields.Date(string='StartDate',default=fields.Date.today)
+    start_date = fields.Datetime(string='StartDate', default=fields.Date.today)
+    end_date = fields.Datetime(string='EndDate', compute='_compute_end_date')
     duration = fields.Integer(string='Duration in Minutes')
     seats = fields.Integer(string='No of Seats')
     course_id = fields.Many2one(comodel_name='course', ondelete='cascade')
-    instructor_id = fields.Many2one(comodel_name='res.partner',ondelete='set null')
-    attendees = fields.Many2many(comodel_name='res.partner',relation='session_attendees')
+    instructor_id = fields.Many2one(comodel_name='res.partner', ondelete='set null')
+    attendees = fields.Many2many(comodel_name='res.partner', relation='session_attendees')
+
+    description = fields.Html(string='Description',related="course_id.description")
+
+    seats_taken = fields.Integer(string="Seats taken", compute='_compute_seats_taken', store=True)
+    is_full = fields.Boolean(string="Course is Full", compute='_compute_is_full',store=True)
+
+
+    @api.depends('start_date')
+    def _compute_end_date(self):
+        for record in self:
+            record.end_date = record.start_date + timedelta(minutes=record.duration)
+
+    @api.depends('attendees')
+    def _compute_seats_taken(self):
+        for record in self:
+            if record.attendees:
+                record.seats_taken = len(record.attendees) / record.seats * 100
+            else:
+                record.seats_taken = 0
+
+    @api.depends('attendees')
+    def _compute_is_full(self):
+        for record in self:
+            record.is_full = len(record.attendees) == record.seats
